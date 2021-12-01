b924628b4bb15a58a43568fa22624ebc57884e90
SoyJRBA <jesusrobertobarreraalvarado@gmail.com>
2021-12-01 06:20:27 -0600

    Respaldo del proyecto CRUD

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,20 @@
 const express = require ('express');
 const router = express.Router();
-const model = require('../models/registro')();
-router.get('/', (req,res) => {
-    res.render('index.ejs');
+const model = require('../models/valores')();
+
+const Valor = require ('../models/valores');
+
+router.get('/', async (req,res) => {
+    const valores = await Valor.find();
+    console.log(valores);
+    res.render('index.ejs',{
+        valores
     });
+});
+
+router.post('/add', async (req,res) => {
+    const valor = new Valor(req.body);
+    await valor.save();
+    res.redirect('/');
+});
 module.exports = router;
